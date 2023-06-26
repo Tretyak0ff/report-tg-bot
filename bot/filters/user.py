bdbb4ddb4f3e3c77f7a02a49eaee67223600190f
Tretyak0ff <evgentretyakoff@gmail.com>
2023-06-26 23:57:01 +0300

    refactoring add work mode

---
+++ bot/filters/user.py
@@ -0,0 +1,22 @@
+from typing import Union, Dict, Any
+from aiogram.filters import BaseFilter
+from aiogram.types import CallbackQuery
+from sqlalchemy.ext.asyncio import AsyncSession
+from services.database import _get_or_create_user
+
+
+class WorkMode(BaseFilter):
+    async def __call__(self, callback: CallbackQuery,
+                       session: AsyncSession) -> Union[bool, Dict[str, Any]]:
+        user = await _get_or_create_user(aiogram_user=callback.from_user,
+                                         session=session)
+        if user.work_mode:
+            return True
+
+
+class AbsenceWorkMode(BaseFilter):
+    async def __call__(self, callback: CallbackQuery,
+                       session: AsyncSession) -> Union[bool, Dict[str, Any]]:
+        user = await _get_or_create_user(aiogram_user=callback.from_user,
+                                         session=session)
+        return {"user": user}
---
+++ bot/services/database.py
@@ -0,0 +1,38 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from aiogram.types.user import User as AiogramUser
+from models.database import User
+from datetime import datetime
+
+
+async def _create_user(aiogram_user: AiogramUser) -> User:
+    new_user = User(telegram_user_id=aiogram_user.id,
+                    created_at=datetime.now(),
+                    first_name=aiogram_user.first_name,
+                    last_name=aiogram_user.last_name,
+                    username=aiogram_user.username,
+                    work_mode=None)
+    return new_user
+
+
+async def _update_user(user: User, aiogram_user: AiogramUser) -> User:
+    update_user = User(id=user.id,
+                       telegram_user_id=user.telegram_user_id,
+                       created_at=user.created_at,
+                       first_name=aiogram_user.first_name,
+                       last_name=aiogram_user.last_name,
+                       username=aiogram_user.username,
+                       work_mode=user.work_mode)
+    return update_user
+
+
+async def _get_or_create_user(aiogram_user: AiogramUser,
+                              session: AsyncSession) -> User:
+    user = await(session.scalar(select(User).where(id == id)))
+    if user:
+        user = await _update_user(user=user, aiogram_user=aiogram_user)
+        await session.merge(user)
+    else:
+        user = await _create_user(aiogram_user=aiogram_user)
+        session.add(user)
+    return user
